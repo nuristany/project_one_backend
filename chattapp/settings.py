813dc3dfeced46cf38763ba45cdd1ce33763493e
Qais <nuristanyqais@gmail.com>
2025-05-07 10:05:55 +0430

    cors modified

--- chattapp/settings.py
+++ chattapp/settings.py
@@ -21,7 +21,7 @@ import os
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "").split() or []
 CSRF_TRUSTED_ORIGINS = os.getenv("DJANGO_CSRF_TRUSTED_ORIGINS", "").split() or []
-CORS_ALLOWED_ORIGINS = os.getenv("DJANGO_CORS_ALLOWED_ORIGINS", "").split() or []
+CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "").split() or []
 
 # Application definition
 
